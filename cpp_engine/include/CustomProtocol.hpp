59f56b1fd3f2a94f1fd8281cd9e98c5f2aadd78c
2002utkarsh <2000utkarsh2002@gmail.com>
2025-10-17 16:50:12 -0600

    fix: correct pragma pack alignment in network header

--- cpp_engine/include/CustomProtocol.hpp
+++ cpp_engine/include/CustomProtocol.hpp
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stddef.h>
 namespace engine { namespace network {
+#pragma pack(push, 1)
 struct PacketHeader {
     uint64_t sequence_id;
     uint8_t ack_flag;
@@ -11,5 +12,12 @@ struct PacketHeader {
     uint32_t checksum;
     uint8_t payload[]; 
 };
+#pragma pack(pop)
+static_assert(sizeof(PacketHeader) == 16, "PacketHeader struct alignment mismatch");
+static_assert(offsetof(PacketHeader, sequence_id) == 0, "offset mismatch");
+static_assert(offsetof(PacketHeader, ack_flag) == 8, "offset mismatch");
+static_assert(offsetof(PacketHeader, payload_length) == 9, "offset mismatch");
+static_assert(offsetof(PacketHeader, reserved) == 11, "offset mismatch");
+static_assert(offsetof(PacketHeader, checksum) == 12, "offset mismatch");
 }}
 #endif
